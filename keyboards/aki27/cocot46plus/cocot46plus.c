e47b46dbcd8479858a444b6de00b9fc9afe28ba5
l3ickey <bickey_t0mat0@outlook.jp>
2025-03-26 12:15:16 +0900

    modified trackball sensitivity

--- keyboards/aki27/cocot46plus/cocot46plus.c
+++ keyboards/aki27/cocot46plus/cocot46plus.c
@@ -26,7 +26,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #ifndef COCOT_CPI_OPTIONS
 //#    define COCOT_CPI_OPTIONS { 250, 500, 750, 1000, 1250 }
-#    define COCOT_CPI_OPTIONS { 250, 1250 }
+#    define COCOT_CPI_OPTIONS { 250, 1000 }
 #endif
 #ifndef COCOT_CPI_DEFAULT
 #    define COCOT_CPI_DEFAULT 1
